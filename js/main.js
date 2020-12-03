252fafef653315d257b060eab309d8a05978474a
Andrius Kairys <andrius.kairys.lt@gmail.com>
2020-12-03 02:37:05 +0200

    added fonts

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 //DATA
 const seq = document.querySelector('div.seq > form');
+const hat = new Audio('./audio/hh.mp3')
+let stepInput = document.querySelector('input[name="stepcount"]');
+const playButton = document.querySelector('.controls button');
 
 
-let BPM = document.querySelector('.controls input').value;
+
+let BPM = 0;
 let tempo = 0;
 let stepAmount = 0;
 let steps = [];
 
+
+
+
 //STEP COUNT
 function stepCount() {
-    const stepInput = document.querySelector('input[name="stepcount"]');
+    stepInput = document.querySelector('input[name="stepcount"]');
     stepAmount = parseFloat(stepInput.value);
     let HTML = '';
     for (i = 1; i <= stepAmount; i++) {
@@ -22,17 +29,19 @@ function stepCount() {
     steps = document.querySelectorAll('label.step');
 }
 
-console.log(parseFloat(document.querySelector('input[name="stepcount"]').value));
-
 //RUN SEQ LOOP
 function runSeq() {
+    stepInput.setAttribute("disabled", true);
+    playButton.setAttribute("disabled", true);
     BPM = document.querySelector('.controls input').value;
     tempo = (240 / BPM) * 1000;
     stepCount();
+    let stepTime = tempo / stepAmount
     let currentStep = 0;
     let prevStep = 0;
         setInterval(
-            function playLoop() {
+            function() {
+                let step = document.querySelectorAll('label.step > input');
                 if (currentStep === 0) {
                     prevStep = stepAmount - 1;
                 }
@@ -48,6 +57,14 @@ function runSeq() {
                 }
                 steps[currentStep].style.backgroundColor = 'orange';
                 steps[prevStep].style.backgroundColor = 'black';
+                if (step[currentStep].checked) {
+                    hat.play();
+                        setTimeout(() => {
+                            hat.pause();
+                            hat.load();
+                        }, stepTime / 1.5
+                    )
+                }
                 currentStep += 1;
                 if (currentStep > stepAmount - 1) {
                     currentStep = 0;
@@ -57,8 +74,6 @@ function runSeq() {
                     prevStep = 0;
                 }
             },
-            tempo / stepAmount
+            stepTime
         );
 }
-
-
