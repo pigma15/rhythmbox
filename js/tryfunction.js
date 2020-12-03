a8964af9c91c9df10eebfa90ca54a8dd0c58194a
Andrius Kairys <andrius.kairys.lt@gmail.com>
2020-12-03 15:49:17 +0200

    better

--- js/tryfunction.js
+++ js/tryfunction.js
@@ -1,3 +1,6 @@
+//AUDIO
+const primaAudio = new Audio('./audio/hh.mp3')
+
 //DATA COMMON
 const playButton = document.querySelector('.controls button[name="play"]');
 const stopButton = document.querySelector('.controls button[name="stop"]');
@@ -6,9 +9,7 @@ let tempo = 0;
 let stepInput = {} ;
 let stepAmount = 0;
 let steps = [];
-
-
-
+let playState = false;
 
 //DATA PRIMA
 const primaSeq = document.querySelector('#prima div.seq > form');
@@ -32,51 +33,64 @@ function primaStepCount() {
     stepCount('prima', primaSeq);
 };
 
+
+
+
+
 //PRIMA RUN SEQ LOOP
-function primaRunSeq() {
-    primaStepCount()
-    primaStepInput.setAttribute("disabled", true);
-    primaSteps = document.querySelectorAll('#prima label.step');
-    const stepTime = tempo / primaStepAmount; 
-    let step = document.querySelectorAll('#prima label.step input');
+function runSeq(track, audio) {
+    playState = true;
+    stepInput = document.querySelector(`#${track} input[name="stepcount"]`);
+    stepAmount = parseFloat(stepInput.value);
+    steps = document.querySelectorAll(`#${track} label.step`);
+    let step = document.querySelectorAll(`#${track} label.step input`);
+    const stepTime = tempo / stepAmount;
     let currentStep = 0;
-    let prevStep = 0;   
+    let prevStep = 0;
+    let sequencer =        
         setInterval(
             function() {
                 if (currentStep === 0) {
-                    prevStep = primaStepAmount - 1;
+                    prevStep = stepAmount - 1;
                 }
                 if(currentStep > 0) {
                     prevStep = currentStep - 1;
                 }
-                if (primaSteps[prevStep] === undefined || primaSteps[currentStep] === undefined) {
-                    for (i = 1; i < primaStepAmount; i++) {
-                        primaSteps[i].style.backgroundColor = 'black';
+                if (steps[prevStep] === undefined || steps[currentStep] === undefined) {
+                    for (i = 1; i < stepAmount; i++) {
+                        steps[i].style.backgroundColor = 'black';
                     }
-                    prevStep = primaStepAmount - 1;
+                    prevStep = stepAmount - 1;
                     currentStep = 0;
                 }
-                primaSteps[currentStep].style.backgroundColor = 'orange';
-                primaSteps[prevStep].style.backgroundColor = 'black';
+                steps[currentStep].style.backgroundColor = 'orange';
+                steps[prevStep].style.backgroundColor = 'black';
                 if (step[currentStep].checked) {
-                    primaAudio.play();
+                    audio.play();
                         setTimeout(() => {
-                            primaAudio.pause();
-                            primaAudio.load();
+                            audio.pause();
+                            audio.load();
                         }, stepTime / 1.5
                     )
                 }
                 currentStep += 1;
-                if (currentStep > primaStepAmount - 1) {
+                if (currentStep > stepAmount - 1) {
                     currentStep = 0;
                 }
                 prevStep += 1;
-                if (prevStep > primaStepAmount - 1) {
+                if (prevStep > stepAmount - 1) {
                     prevStep = 0;
+                }
+                if (playState === false ) {
+                    clearInterval(sequencer);
+                    for (i = 1; i < stepAmount; i++) {
+                        steps[i].style.backgroundColor = 'black';
+                    }
                 }
             },
             stepTime
         );
+
 }
 
 //TRANSPORT
@@ -85,5 +99,61 @@ function play() {
     stopButton.removeAttribute("disabled")
     BPM = document.querySelector('.controls input').value;
     tempo = (240 / BPM) * 1000;
-    primaRunSeq();
+    runSeq('prima', primaAudio)
 }
+
+function stop() {
+    stopButton.setAttribute("disabled", true);
+    playButton.removeAttribute("disabled")    
+    playState = false
+}
+
+
+
+/*
+//PRIMA RUN SEQ LOOP
+function runSeq(track, audio,) {
+    stepInput = document.querySelector(`#${track} input[name="stepcount"]`);
+    stepAmount = parseFloat(stepInput.value);
+    steps = document.querySelectorAll(`#${track} label.step`);
+    let step = document.querySelectorAll(`#${track} label.step input`);
+    const stepTime = tempo / stepAmount;
+    let currentStep = 0;
+    let prevStep = 0;
+        setInterval(
+            function() {
+                if (currentStep === 0) {
+                    prevStep = stepAmount - 1;
+                }
+                if(currentStep > 0) {
+                    prevStep = currentStep - 1;
+                }
+                if (steps[prevStep] === undefined || steps[currentStep] === undefined) {
+                    for (i = 1; i < stepAmount; i++) {
+                        steps[i].style.backgroundColor = 'black';
+                    }
+                    prevStep = stepAmount - 1;
+                    currentStep = 0;
+                }
+                steps[currentStep].style.backgroundColor = 'orange';
+                steps[prevStep].style.backgroundColor = 'black';
+                if (step[currentStep].checked) {
+                    audio.play();
+                        setTimeout(() => {
+                            audio.pause();
+                            audio.load();
+                        }, stepTime / 1.5
+                    )
+                }
+                currentStep += 1;
+                if (currentStep > stepAmount - 1) {
+                    currentStep = 0;
+                }
+                prevStep += 1;
+                if (prevStep > stepAmount - 1) {
+                    prevStep = 0;
+                }
+            },
+            stepTime
+        );
+} */
